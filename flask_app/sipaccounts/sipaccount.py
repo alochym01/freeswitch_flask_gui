70c45240be81cb824c167939ebb84a8cceae65b5
Alochym01 <hadn@ubuntu>
2018-11-12 22:50:44 +0700

    update remove redis record for sip account

--- flask_app/sipaccounts/sipaccount.py
+++ flask_app/sipaccounts/sipaccount.py
@@ -4,6 +4,7 @@ from flask_app.models.sip_account import SipAcc
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required
 from flask_app import db
+import redis
 
 @sip_account.route('/')
 @login_required
@@ -68,8 +69,8 @@ def edit(id):
 @sip_account.route('/update/<int:id>', methods=['POST'])
 @login_required
 def update(id):
-    form = SipAccount()    
-    sipacc = SipAcc.query.filter_by(id=id).first()    
+    form = SipAccount()
+    sipacc = SipAcc.query.filter_by(id=id).first()
     if form.validate_on_submit():
         sipacc.username=form.username.data
         sipacc.domain=form.domain.data
@@ -82,6 +83,13 @@ def update(id):
         sipacc.outbound_caller_name=form.outbound_caller_name.data
         sipacc.set_password(form.password.data)
         db.session.commit()
+        redis_key = sipacc.username + '_' + sipacc.domain
+        try:
+            r = redis.StrictRedis(host='localhost', port=6379, db=0)
+            r.delete(redis_key)
+            r.connection_pool.disconnect()
+        except:
+            pass
         flash('Update successfully')
         return redirect(url_for('sip-account.index'))
     return render_template('sipaccs/edit.html', sipacc=sipacc, form=form)
@@ -93,6 +101,13 @@ def delete(id):
     sipacc = SipAcc.query.filter_by(id=id).first()
     if request.method == 'GET':
         return render_template('sipaccs/delete.html', sipacc=sipacc, form=form)
+    redis_key = sipacc.username + '_' + sipacc.domain
+    try:
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        r.delete(redis_key)
+        r.connection_pool.disconnect()
+    except:
+        pass
     db.session.delete(sipacc)
     db.session.commit()
     flash('Delete successfully')
